67db7ff1ca775dd22b9eba62a52ecc627b85e518
Jonathan <jntdhow13@gmail.com>
2021-12-03 07:23:04 -0300

    Implementacao do cadastro de usuarios

---
+++ src/main/java/io/github/jonathanSampaio/rest/controller/UsuarioController.java
@@ -0,0 +1,30 @@
+package io.github.jonathanSampaio.rest.controller;
+
+import io.github.jonathanSampaio.domain.entity.Usuario;
+import io.github.jonathanSampaio.service.impl.UsuarioServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioServiceImpl usuarioService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public Usuario salvar(@RequestBody @Valid Usuario usuario){
+        String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(senhaCriptografada);
+        return usuarioService.salvar(usuario);
+    }
+
+
+}
