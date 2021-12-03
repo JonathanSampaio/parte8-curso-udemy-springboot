2fd53d5d33dae4a6d080fb3b64e8c76856b164e0
Jonathan <jntdhow13@gmail.com>
2021-12-03 15:30:25 -0300

    autenticacao com cadastro de usuarios

--- src/main/java/io/github/jonathanSampaio/config/SecurityConfig.java
+++ src/main/java/io/github/jonathanSampaio/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package io.github.jonathanSampaio.config;
 import io.github.jonathanSampaio.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -40,6 +41,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .hasAnyRole("USER","ADMIN")
                 .antMatchers("/api/produtos/**")
                         .hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST,"/api/usuarios/**")
+                        .permitAll()
+                .anyRequest().authenticated()
             .and()
                 .httpBasic()
         ;
