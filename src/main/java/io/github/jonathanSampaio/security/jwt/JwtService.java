86cdfac0b41b2c770b2388899d849b8a146d5e28
Jonathan <jntdhow13@gmail.com>
2021-12-10 10:32:00 -0300

    implementando o filtro do jwt

--- src/main/java/io/github/jonathanSampaio/security/jwt/JwtService.java
+++ src/main/java/io/github/jonathanSampaio/security/jwt/JwtService.java
@@ -1,5 +1,6 @@
-package io.github.jonathanSampaio;
+package io.github.jonathanSampaio.security.jwt;
 
+import io.github.jonathanSampaio.VendasApplication;
 import io.github.jonathanSampaio.domain.entity.Usuario;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
@@ -14,7 +15,6 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
-import java.util.HashMap;
 
 @Service
 public class JwtService {
